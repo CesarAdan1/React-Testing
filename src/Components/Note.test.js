b4031589b7f7e8d7b462a83d40e9703504a8cb0c
CesarAdan1 <whoclash@hotmail.com>
2019-01-30 16:51:37 -0600

    testingthenotecomponent

---
+++ src/Components/Note.test.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { mount } from 'enzyme';
+import Note from './Note';
+
+//passing props
+const props = { note: { text: 'test note'}}
+//specify the descirption of the test
+describe('Note', () => {
+    //spread atributes
+    let note = mount(<Note {...props}/>);
+
+    //decribe the test
+    it('renders the note text', () => {
+        console.log(note.debug());
+        expect(note.find('p').text()).toEqual(props.note.text);
+    });
+});
