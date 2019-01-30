c61450c91406a5acc4cb316679273282d7d5ee80
CesarAdan1 <whoclash@hotmail.com>
2019-01-30 13:54:34 -0600

    createbootstrapfrom

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App, { color, number } from './Components/App';
+import App from './Components/App';
 import './index.css';
 
-console.log(color, number)
-//what to render/ where
+
 ReactDOM.render(
     <App />,
 document.getElementById('root'));
