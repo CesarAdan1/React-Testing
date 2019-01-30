5419becf3b47ad477e6aaba3aafe2bc792b84915
CesarAdan1 <whoclash@hotmail.com>
2019-01-30 15:55:15 -0600

    propsstylescreatenotecomponentmodularize

---
+++ src/Components/Note.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import '../index.css';
+
+class Note extends Component {
+  render() {
+    return (
+      <div className='note'>
+        <p>{this.props.note.text}</p>
+      </div>
+    )
+  }
+}
+export default Note;
