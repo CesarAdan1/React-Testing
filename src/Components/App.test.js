56fb317651f5055278c006909cc5e18d7f329890
CesarAdan1 <whoclash@hotmail.com>
2019-01-30 20:39:04 -0600

    testinbehaviour

--- src/Components/App.test.js
+++ src/Components/App.test.js
@@ -33,4 +33,41 @@ describe('App', () => {
             expect(app.find('.btn').at(0).text('Submit'));
         });
     });
+
+    //Testing from behaviour
+    describe('when creating a note', () => {
+        let testNote = 'test note';
+
+        beforeEach(() => {
+            app.find('FormControl').simulate('change',{
+                //An object
+                target: { value: testNote }
+            });
+        });
+
+        it('updates the text in state', () => {
+            expect(app.state().text).toEqual(testNote);
+        });
+
+        describe('and submitting the new note', () => {
+            beforeEach(() => {
+               app.find('.btn').at(0).simulate('click'); 
+            });
+
+            it('adds the new state', () => {
+                //console.log(app.state());
+                expect(app.state().notes[0].text).toEqual(testNote);
+            });
+
+            describe('and clicking th clear button', () => {
+                beforeEach(() => {
+                    app.find('.btn').at(1).simulate('click');
+                });
+
+                it('clears the notes in state', () => {
+                    expect(app.state().notes).toEqual([]);
+                });
+            });
+        });
+    });
 });
